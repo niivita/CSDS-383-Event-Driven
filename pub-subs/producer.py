cbb1eda27735235cfd898ae60885c5a20ab5185c
Nivita <nnp25@case.edu>
2023-11-10 17:56:44 -0500

    refactored methods

--- pub-subs/producer.py
+++ pub-subs/producer.py
@@ -5,30 +5,23 @@ import pika
 # (install with brew) (add to path)
 # sudo rabbitmq-server
 
+connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
+channel = connection.channel()
+channel.exchange_declare(exchange='exchange', exchange_type='topic')
+
 
 # method to be called with some dictionary of information
 def publish_event(event_body):
     # establish a connection
-    connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
-    channel = connection.channel()
-
-    channel.exchange_declare(exchange='exchange', exchange_type='topic')
-
     message = json.dumps(event_body)
 
     # exchange: tldr- a mediator before queue
     # routing_key: the topic
-    channel.basic_publish(exchange='exchange', routing_key='events', body=message)
     print(f" [x] Sent {message}")
     connection.close()
 
 def publish_participant(event_body):
     # establish a connection
-    connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
-    channel = connection.channel()
-
-    channel.exchange_declare(exchange='exchange', exchange_type='topic')
-
     message = json.dumps(event_body)
 
     # exchange: tldr- a mediator before queue
@@ -36,7 +29,6 @@ def publish_participant(event_body):
     channel.basic_publish(exchange='exchange', routing_key='participants', body=message)
     print(f" [x] Sent {message}")
     connection.close()
-
 
 # store a fake event (placeholder for input
 fake_event = {"UUID": "1234567",
