fa9a13a97023200b57473ca3b07780ebf385d0cc
alp133 <102556053+anaperezz@users.noreply.github.com>
2023-11-12 12:35:58 -0500

    inserting events

--- event-driven-components/consumer.py
+++ event-driven-components/consumer.py
@@ -28,17 +28,23 @@ def callback(ch, method, properties, body):
     time.sleep(body.count(b'.'))
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-
-    # TODO: FOR ANA AND CALLIE
-    # method.routing_key.split('_')[0] will be either "events" or "participants":
-    # use routing key to see if is "events", then send to the events database, else send to participants
-    url= "https://t6r6u8jln4.execute-api.us-east-1.amazonaws.com/main/participants/?"
+    #Insertion calling API
+    participant_url= "https://t6r6u8jln4.execute-api.us-east-1.amazonaws.com/main/participants/?"
+    event_url= "https://t6r6u8jln4.execute-api.us-east-1.amazonaws.com/main/events/?"
+    #participants
     if method.routing_key.split(' ')[0] == "participants_queue":
         print("Inserting participant:")
-        data = json.loads(body.decode('UTF-8'))
-        print(dict(data))
-        x = requests.post(url+urllib.parse.urlencode(data))
-        print(x)
+        params = json.loads(body.decode('UTF-8'))
+        #posting using url params
+        response = requests.post(participant_url+urllib.parse.urlencode(params))
+        print(str(response) + ": Participant added successfully")
+    #events
+    elif method.routing_key.split(' ')[0] == "events_queue":
+        print("Inserting event:")
+        params = json.loads(body.decode('UTF-8'))
+        #posting using url params
+        response = requests.post(event_url+urllib.parse.urlencode(params))
+        print(str(response) + ": Event added successfully")
     else:
         print("Not inserting anything")
         print(method.routing_key.split(' '))
